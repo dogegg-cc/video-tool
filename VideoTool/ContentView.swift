4d0464207902ba7cd51b190bd61aa7df099dae15
Jiao <343001118@qq.com>
2026-05-27 10:11:53 +0800

    Initial Commit

---
+++ VideoTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoTool
+//
+//  Created by 我勒个去去 on 2026/5/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoTool/VideoToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoToolApp.swift
+//  VideoTool
+//
+//  Created by 我勒个去去 on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct VideoToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
